7636e550f29f5ada7e6b2b36081942d1703d2316
fayezosaadi <26156279+fayezosaadi@users.noreply.github.com>
2026-08-14 14:38:03 -0600

    test external tenant

--- versions.tf
+++ versions.tf
@@ -12,11 +12,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.37"
+      version = "~> 5.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 3.6.0"
+      version = "~> 3.0.0"
     }
     random = {
       source  = "hashicorp/random"
