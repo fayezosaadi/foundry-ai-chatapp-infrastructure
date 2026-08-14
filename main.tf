f8770774b1c3fdf250a5a60ded911733a7370805
fayezosaadi <26156279+fayezosaadi@users.noreply.github.com>
2026-08-14 16:09:07 -0600

    add rg

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ module "metadata" {
   source      = "git::https://github.com/fayezosaadi/techforlife-azure-terraform-labels.git?ref=06fe850743d945f5c10494dcdf5d1961300b10d3"
   environment = local.environment
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.rg_name
+  location = local.location
+
+  tags = merge(module.metadata.tags, { what_is_this = "ChatApp Infrastructure Resource Group" })
+}
